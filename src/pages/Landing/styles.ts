19f6f0ad53ec96bc9dd83eaeacdded3762f1802f
Lucas Del Puerto <lucasgarciadragon@hotmail.com>
2020-08-09 18:15:43 -0300

    :art: Create landing page

--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -23,7 +23,46 @@ const styles = StyleSheet.create({
 
     titleBold: {
         fontFamily: 'Poppins_600SemiBold'
+    },
+
+    buttonsContainer: {
+        flexDirection: 'row',
+        marginTop: 40,
+        justifyContent: "space-between",
+    },
+
+    button: {
+        height: 120,
+        width: '48%',
+        backgroundColor: '#333',
+        borderRadius: 8,
+        padding: 24,
+        justifyContent: 'space-between'
+    },
+
+    buttonPrimary: {
+        backgroundColor: '#9871f5'
+    },
+
+    buttonSecondary: {
+        backgroundColor: '#04d361'
+    },
+
+    buttonText: {
+        fontFamily: 'Archivo_700Bold',
+        color: '#FFF',
+        fontSize: 16
+    },
+
+    totalConnections: {
+        fontFamily: 'Poppins_400Regular',
+        color: '#d4c2ff',
+        fontSize: 12,
+        lineHeight: 20,
+        maxHeight: 140,
+        marginTop: 30,
     }
+
 });
 
 export default styles;
